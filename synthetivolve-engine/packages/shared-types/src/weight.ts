8cc0449b8ba9f3785f082a3ca0315482e59f3f93
bejimenez <30389918+bejimenez@users.noreply.github.com>
2025-06-26 14:48:00 -0700

    Delete synthetivolve-engine directory

--- synthetivolve-engine/packages/shared-types/src/weight.ts
+++
@@ -1,5 +0,0 @@
-// packages/shared-types/src/weight.ts
-import type { Database } from './database.types';
-
-// This is the clean type for a weight entry
-export type WeightEntry = Database['public']['Tables']['weight_entries']['Row'];
